f9ef6780bc7d343acc75044e44aac66013929d02
Catmaster543 <mastercat543@gmail.com>
2026-06-17 12:26:31 +0200

    Added description
    - Added the option to add description to each component
    - Added description to show up in detail window

--- Pc parts lister/MainWindow.xaml.cs
+++ Pc parts lister/MainWindow.xaml.cs
@@ -58,6 +58,7 @@ namespace Pc_parts_lister
     public class Component
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public string Type { get; set; }
         public string Manufacturer { get; set; }
         public string Series { get; set; }
